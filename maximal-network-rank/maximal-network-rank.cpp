e96ef352fc9d24b9e0098bca32b6fd2bd02db341
Raj Chaurasia <67376539+RajChaurasia1@users.noreply.github.com>
2021-10-12 15:10:08 +0530

    Time: 96 ms (62.92%), Space: 36 MB (44.58%) - LeetHub

--- maximal-network-rank/maximal-network-rank.cpp
+++ maximal-network-rank/maximal-network-rank.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int>adj[n];
+        vector<vector<int>>ed(n,vector<int>(n,0));
         int ans = 0;
+        
+        
         
         for(auto road : roads){
             adj[road[0]].push_back(road[1]);
             adj[road[1]].push_back(road[0]);
+            
+            ed[road[0]][road[1]]++,ed[road[1]][road[0]]++;
         }
         
         
         for(int i = 0 ; i < n ; i++){
             for(int j = i + 1 ; j < n ; j++){
-                int tot = adj[i].size() + adj[j].size();
-                for(auto v : adj[j]){
-                    if(v == i){
-                        --tot;
-                        break;
-                    }
-                }
+                int tot = adj[i].size() + adj[j].size() - ed[i][j];
+              
                 ans = max(ans,tot);
             }
         }
