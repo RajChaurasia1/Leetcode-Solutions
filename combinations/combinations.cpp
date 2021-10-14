6f93a4dd6ec5fef2c3197863ded1ec9b2e637b02
Raj Chaurasia <67376539+RajChaurasia1@users.noreply.github.com>
2021-10-14 09:42:32 +0530

    Time: 33 ms (47.98%), Space: 9.8 MB (61.63%) - LeetHub

---
+++ combinations/combinations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void recurse(int i, int k, int n,vector<int>&v){
+        if(k < 0){
+            return;
+        }
+        
+        if(k == 0){
+            ans.push_back(v);
+            return;
+        }
+            
+        for(int j = i ; j <= n ; j++){
+            v.push_back(j);
+            recurse(j + 1, k - 1, n, v);
+            v.pop_back();
+        }
+            
+    }
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        
+        for(int i = 1 ; i <= n ; i++){
+            vector<int>v;
+            v.push_back(i);
+            recurse(i + 1,k - 1,n,v);
+        }
+        return ans;
+        
+    }
+};
