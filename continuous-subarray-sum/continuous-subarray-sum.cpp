6388df5e052d0c552a5edd1d60593f349638d2ef
Raj Chaurasia <67376539+RajChaurasia1@users.noreply.github.com>
2021-10-31 16:02:39 +0530

    Time: 160 ms (89.05%), Space: 96.5 MB (51.77%) - LeetHub

---
+++ continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+     unordered_map<int,int>mp;
+        
+        int sum = 0;
+        
+     for(int i = 0 ; i < nums.size() ; i++){
+         sum += nums[i];
+         
+         sum %= k;
+         
+         if(sum == 0 && i)
+            return true;
+         
+         if(mp.find(sum) != mp.end()){
+             if(i - mp[sum] > 1)
+                return true;
+         }
+         else
+            mp[sum] = i;
+     }
+        
+        return false;
+        
+    }
+};
